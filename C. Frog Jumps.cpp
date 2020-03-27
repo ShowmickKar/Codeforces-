8144434dfa9323a6b0c42668f09253f7f43f6436
Showmick Kar <62438629+ShowmickKar@users.noreply.github.com>
2020-03-27 10:56:43 +0600

    Add files via upload

---
+++ C. Frog Jumps.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    /*int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        vector <int> a(s.size());
+        a.push_back(0);
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'R')
+                a.push_back(i + 1);
+        }
+
+        a.push_back(s.size() + 1);
+        int m = 0;
+        for(int i = 0; i < a.size() - 1; i++){
+            m = max(m, a[i + 1] - a[i]);
+        }
+
+        cout << m << endl;
+    }*/
+
+    int s = pow(2, 3);
+    cout << s << endl;
+
+    return 0;
+}
